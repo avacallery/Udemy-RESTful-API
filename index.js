f828689a110a739cc02dcc49fc1b33193c250a08
avacallery <avacallery@gmail.com>
2019-11-29 18:29:48 -0500

    HTTP methods

--- index.js
+++ index.js
@@ -39,16 +39,9 @@ app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id)); 
     if (!course) return res.status(404).send('The course with the given id was not found.');
     
-
         const { error } = validateCourse(req.body); //only interested in error message so we use object destructuring { error } 
         if (error) return res.status(400).send(error.details[0].message);
          
-        // function validateCourse(course) {
-        //     const schema = { 
-        //         name: Joi.string().min(3).required() 
-        //         }; 
-        //         return Joi.validate(course, schema);
-        // }
 
     //update course
     //return updated course to client
